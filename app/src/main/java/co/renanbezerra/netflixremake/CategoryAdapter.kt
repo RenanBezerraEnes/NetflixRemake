dbe6ca0bf4c05b1252524c6bb729cef43833d759
Renan <renanbezerraenes@icloud.com>
2025-02-10 16:05:08 +0100

    Add categories and movies view, netflix layout with categories and movies

---
+++ app/src/main/java/co/renanbezerra/netflixremake/CategoryAdapter.kt
@@ -0,0 +1,39 @@
+package co.renanbezerra.netflixremake
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.renanbezerra.netflixremake.model.Category
+import co.renanbezerra.netflixremake.model.Movie
+
+class CategoryAdapter(private val categories: List<Category>): RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.category_item, parent, false)
+        return CategoryViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val categories = categories[position]
+        holder.bind(categories)
+    }
+
+    inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(category: Category){
+            val txtTitle:TextView = itemView.findViewById(R.id.txt_title)
+            txtTitle.text = category.name
+
+            val rvCategories: RecyclerView = itemView.findViewById(R.id.rv_category)
+            rvCategories.layoutManager = LinearLayoutManager(itemView.context, RecyclerView.HORIZONTAL, false)
+            rvCategories.adapter = MovieAdapter(category.movies)
+        }
+    }
+}
--- app/src/main/java/co/renanbezerra/netflixremake/MainActivity.kt
+++ app/src/main/java/co/renanbezerra/netflixremake/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import co.renanbezerra.netflixremake.model.Category
 import co.renanbezerra.netflixremake.model.Movie
 
 class MainActivity : AppCompatActivity() {
@@ -12,16 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val movies = mutableListOf<Movie>()
-        for (i in 0 until 5){
-            val movie = Movie(R.drawable.movie)
-            movies.add(movie)
+        val categories = mutableListOf<Category>()
+        for (i in 0 until 5) {
+            val movies = mutableListOf<Movie>()
+            for (i in 0 until 15){
+                val movie = Movie(R.drawable.movie)
+                movies.add(movie)
+            }
+            val category = Category("cat $i", movies)
+            categories.add(category)
         }
 
-        val adapter = MainAdapter(movies)
+        val adapter = CategoryAdapter(categories)
         val rv: RecyclerView = findViewById(R.id.rv_main)
 
-        rv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        rv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         rv.adapter = adapter
     }
 
