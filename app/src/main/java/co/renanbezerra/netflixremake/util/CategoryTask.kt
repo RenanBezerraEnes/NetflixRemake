d78f8a890cdb38785e625373898603d4c5c5f16f
Renan <renanbezerraenes@icloud.com>
2025-02-12 10:42:19 +0100

    Another way to get and convert the data

--- app/src/main/java/co/renanbezerra/netflixremake/util/CategoryTask.kt
+++ app/src/main/java/co/renanbezerra/netflixremake/util/CategoryTask.kt
@@ -1,7 +1,10 @@
 package co.renanbezerra.netflixremake.util
 
 import android.util.Log
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.io.InputStream
 import java.net.URL
 import java.util.concurrent.Executors
 import javax.net.ssl.HttpsURLConnection
@@ -24,11 +27,29 @@ class CategoryTask {
                 }
 
                 val stream = urlConnection.inputStream
-                val jsonAsString = stream.bufferedReader().use { it.readText() } //transform bytes to string
-                Log.i("TESTE",jsonAsString)
+                //val jsonAsString = stream.bufferedReader().use { it.readText() } //transform bytes to string
+                //Log.i("TESTE",jsonAsString)
+
+                val buffer = BufferedInputStream(stream)
+                val jsonAsString = toString(buffer)
+                Log.i("TESTE", jsonAsString)
             } catch (e: IOException) {
                 Log.e("Teste", e.message ?: "Error not recongnize", e)
             }
         }
+    }
+
+    private fun toString(stream: InputStream): String {
+        val bytes = ByteArray(1024)
+        var read: Int
+        var byteArrayOPS = ByteArrayOutputStream()
+        while (true) {
+            read = stream.read(bytes)
+            if(read <= 0){
+                break
+            }
+            byteArrayOPS.write(bytes, 0, read)
+        }
+        return String(byteArrayOPS.toByteArray())
     }
 }
