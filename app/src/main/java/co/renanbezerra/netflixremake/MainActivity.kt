c128388421c0155ec3cd86d3c71abe10e2892043
Renan <renanbezerraenes@icloud.com>
2025-02-12 11:38:56 +0100

    add handler to execute the callback methods in the main thread, add progress bar

--- app/src/main/java/co/renanbezerra/netflixremake/MainActivity.kt
+++ app/src/main/java/co/renanbezerra/netflixremake/MainActivity.kt
@@ -2,17 +2,23 @@ package co.renanbezerra.netflixremake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.renanbezerra.netflixremake.model.Category
-import co.renanbezerra.netflixremake.model.Movie
 import co.renanbezerra.netflixremake.util.CategoryTask
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CategoryTask.CallBack {
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById(R.id.progress_main)
 
         val categories = mutableListOf<Category>()
 
@@ -23,7 +29,21 @@ class MainActivity : AppCompatActivity() {
         rv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         rv.adapter = adapter
 
-        CategoryTask().execute("https://atway.tiagoaguiar.co/fenix/netflixapp/home?apiKey=ce653dcd-037c-4f42-bcf5-539c6a516c3f")
+        CategoryTask(this).execute("https://atway.tiagoaguiar.co/fenix/netflixapp/home?apiKey=ce653dcd-037c-4f42-bcf5-539c6a516c3f")
+    }
+
+    override fun onResult(categories: List<Category>) {
+        Log.i("TESTE", categories.toString())
+        progressBar.visibility = View.GONE
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        progressBar.visibility = View.GONE
+    }
+
+    override fun onPreExecute() {
+       progressBar.visibility = View.VISIBLE
     }
 
 }
--- app/src/main/java/co/renanbezerra/netflixremake/util/CategoryTask.kt
+++ app/src/main/java/co/renanbezerra/netflixremake/util/CategoryTask.kt
@@ -1,6 +1,11 @@
 package co.renanbezerra.netflixremake.util
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import co.renanbezerra.netflixremake.model.Category
+import co.renanbezerra.netflixremake.model.Movie
+import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.ByteArrayOutputStream
 import java.io.IOException
@@ -9,15 +14,27 @@ import java.net.URL
 import java.util.concurrent.Executors
 import javax.net.ssl.HttpsURLConnection
 
-class CategoryTask {
+class CategoryTask(private val callback: CallBack) {
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    interface CallBack {
+        fun onResult(categories: List<Category>)
+        fun onFailure(message: String)
+        fun onPreExecute()
+    }
 
     fun execute(url: String) {
+        callback.onPreExecute()
         val executor = Executors.newSingleThreadExecutor()
 
         executor.execute {
+            var urlConnection: HttpsURLConnection? = null
+            var buffer: BufferedInputStream? = null
+            var stream: InputStream? = null
             try {
                 val requestUrl = URL(url)
-                val urlConnection = requestUrl.openConnection() as HttpsURLConnection
+                urlConnection = requestUrl.openConnection() as HttpsURLConnection
                 urlConnection.readTimeout = 2000
                 urlConnection.connectTimeout = 2000
 
@@ -26,17 +43,60 @@ class CategoryTask {
                     throw IOException("Error in the communication with the server")
                 }
 
-                val stream = urlConnection.inputStream
+                stream = urlConnection.inputStream
                 //val jsonAsString = stream.bufferedReader().use { it.readText() } //transform bytes to string
                 //Log.i("TESTE",jsonAsString)
 
-                val buffer = BufferedInputStream(stream)
+                buffer = BufferedInputStream(stream)
                 val jsonAsString = toString(buffer)
-                Log.i("TESTE", jsonAsString)
+
+                val categories = toCategory(jsonAsString)
+
+                handler.post {
+                    callback.onResult(categories)
+                }
+
             } catch (e: IOException) {
-                Log.e("Teste", e.message ?: "Error not recongnize", e)
+                var messageError = e.message ?: "Error not recongnize"
+                Log.e("Teste", messageError, e)
+
+                handler.post {
+                    callback.onFailure(messageError)
+                }
+            } finally {
+                urlConnection?.disconnect()
+                stream?.close()
+                buffer?.close()
             }
         }
+    }
+
+    private fun toCategory(jsonAsString: String): List<Category> {
+        val categories = mutableListOf<Category>()
+
+        val jsonRoot = JSONObject(jsonAsString)
+
+        val jsonCategories = jsonRoot.getJSONArray("category")
+
+        for (i in 0 until jsonCategories.length()) {
+            val jsonCategory = jsonCategories.getJSONObject(i)
+            val title = jsonCategory.getString("title")
+            val jsonMovies = jsonCategory.getJSONArray("movie")
+
+            val movies = mutableListOf<Movie>()
+
+            for (m in 0 until jsonMovies.length()) {
+                val jsonMovie = jsonMovies.getJSONObject(m)
+                val id = jsonMovie.getInt("id")
+                val coverUrl = jsonMovie.getString("cover_url")
+
+                movies.add(Movie(id, coverUrl))
+            }
+
+            categories.add(Category(title, movies))
+        }
+
+        return categories
     }
 
     private fun toString(stream: InputStream): String {
@@ -45,7 +105,7 @@ class CategoryTask {
         var byteArrayOPS = ByteArrayOutputStream()
         while (true) {
             read = stream.read(bytes)
-            if(read <= 0){
+            if (read <= 0) {
                 break
             }
             byteArrayOPS.write(bytes, 0, read)
